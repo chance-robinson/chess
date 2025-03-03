f94eec04073f2161a4ec97aa3b278abd5bca2982
Chance <brighamyoung2@gmail.com>
2025-03-02 22:41:35 -0700

    local server running

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,22 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        try {
+            var port = 8080;
+            var server = new Server().run(port);
+            System.out.printf("Server started on port %d", port);
+            return;
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
+        System.out.println("""
+                CS240 Server:
+                java ServerMain <port>
+                """);
     }
 }
