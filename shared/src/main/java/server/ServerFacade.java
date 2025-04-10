54de7ed4c0400caffecb81638833b19805c3512d
Chance <brighamyoung2@gmail.com>
2025-04-09 19:02:34 -0600

    moving network request/responses to shared so I can implement serverfacade

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,4 +1,42 @@
 package server;
 
+import server.net.request.RegisterRequest;
+import server.net.result.RegisterResult;
+
 public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public RegisterResult register(RegisterRequest request) {
+        return null;
+    }
+
+//    // Deletes
+//    ClearHandler clearHandler = new ClearHandler(generalService);
+//    delete("/db", clearHandler);
+//
+//    LogoutHandler logoutHandler = new LogoutHandler(userService);
+//    delete("/session", logoutHandler);
+//
+//    // Posts
+//    RegisterHandler registerHandler = new RegisterHandler(userService);
+//    post("/user", registerHandler);
+//
+//    LoginHandler loginHandler = new LoginHandler(userService);
+//    post("/session", loginHandler);
+//
+//    CreateGameHandler createGameHandler = new CreateGameHandler(gameService);
+//    post("/game", createGameHandler);
+//
+//    // Puts
+//    JoinGameHandler joinGameHandler = new JoinGameHandler(gameService);
+//    put("/game", joinGameHandler);
+//
+//    // Gets
+//    ListGamesHandler listGameHandler = new ListGamesHandler(gameService);
+//    get("/game", listGameHandler);
 }
