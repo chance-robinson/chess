4d69a86c752ba19f37e55b065bfd7bda4738844c
Chance <brighamyoung2@gmail.com>
2025-04-10 20:42:50 -0600

    basic structure for how im going to implement chessboard drawing

--- client/src/main/java/ui/client/InGameClient.java
+++ client/src/main/java/ui/client/InGameClient.java
@@ -1,19 +1,51 @@
 package ui.client;
 
+import exception.ResponseException;
+import model.GameData;
 import server.ServerFacade;
+
+import java.util.Arrays;
 
 public class InGameClient implements Client {
     private static ServerFacade serverFacade;
     private String authToken;
     private String playerColor = "WHITE";
+    private GameData gameData = null;
 
     public InGameClient(String serverUrl) {
         serverFacade = new ServerFacade(serverUrl);
     }
 
     @Override
-    public ClientResult eval(String input) {
-        return new ClientResult("inGameEval", null, null);
+    public ClientResult eval(String input) throws ResponseException {
+        var tokens = input.toLowerCase().split(" ");
+        var cmd = (tokens.length > 0) ? tokens[0] : "help";
+        var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+        return switch (cmd) {
+            case "logout" -> logout();
+            case "quit" -> new ClientResult("quit", ClientState.SIGNEDOUT, null);
+            default -> help();
+        };
+    }
+
+    public ClientResult logout() throws ResponseException {
+        try {
+            serverFacade.logout(authToken);
+            return new ClientResult("logout", ClientState.SIGNEDOUT, null);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ClientResult help() {
+        String helpText =
+                """
+                logout - when you are done
+                quit - playing chess
+                help - with possible commands
+                """;
+        System.out.println(helpText);
+        return new ClientResult("help", null, null);
     }
 
     public void setAuthToken(String token) {
